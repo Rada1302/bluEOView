2a9118def626438021e69556276b64b1f58ed1a8
rkamysheva <rada.kamysheva@inf.ethz.ch>
2025-11-01 12:47:38 +0100

    update references and data courtesy

--- frontend/src/components/ReferencesModal.js
+++ frontend/src/components/ReferencesModal.js
@@ -7,15 +7,31 @@ import {
   Typography,
   Link,
   Button,
+  List,
+  ListItem,
 } from '@mui/material';
-// TODO: Fill in references content
 
 const ReferencesModal = ({ open, onClose }) => (
-  <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
+  <Dialog open={open} onClose={onClose}>
     <DialogTitle>References &amp; Data Courtesy</DialogTitle>
 
     <DialogContent dividers>
-
+      <Typography variant="body1">
+        Schickele, A., Clerc, C., Benedetti, F., De Angelis, D., Hofmann Elizondo, U., Münnich, M.,
+        Irisson, J.-O., &amp; Vogt, M. (2025).{<br></br>}
+        <i>
+          CEPHALOPOD: A package to standardize marine habitat-modelling practices and enhance
+          inter-comparability across biological observations
+        </i>.{' '}
+        <em>Methods in Ecology and Evolution</em>.{' '}
+        <Link
+          href="https://doi.org/10.1111/2041-210X.70040"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://doi.org/10.1111/2041-210X.70040
+        </Link>
+      </Typography>
     </DialogContent>
 
     <DialogActions>
